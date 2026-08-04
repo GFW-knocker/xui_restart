b83893529c6e6990e1cdc72e4866b5996ac80e64
GFW-knocker <123458121+GFW-knocker@users.noreply.github.com>
2026-08-04 12:07:11 +0330

    Update xui_restart.py

--- xui_restart.py
+++ xui_restart.py
@@ -1,10 +1,12 @@
 import time
 import datetime
 import os
-import psutil    # --> pip install psutil
-from pytz import timezone    # --> pip install pytz
+import psutil    # --> (sudo apt-get install python3-psutil) or (pip install psutil)
+from pytz import timezone    # --> (sudo apt-get install python3-pytz) or (pip install pytz)
 
 
+mem_thr = 93
+cpu_thr = 99.7
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -12,14 +14,28 @@ file_path = os.path.join(dir_path, 'restart_log.txt')
 
 t1 = datetime.datetime.now(timezone('Asia/Tehran'))
 
+
+psutil.cpu_percent(interval=None)
+
+print("start monitoring cpu & ram ...")
+
+
 while(True):
-    time.sleep(10)
+    p_cpu = psutil.cpu_percent(interval=20) # block 20 sec and average cpu over that time
     p_mem = psutil.virtual_memory().percent
-    #print(p_mem)
-    if(p_mem > 90):
+
+    # print(p_cpu)
+    # print(p_mem)
+    # print("-------")
+
+    
+    if( (p_mem > mem_thr) or (p_cpu > cpu_thr) ):
         with open(file_path, 'a') as f:
             t2 = datetime.datetime.now(timezone('Asia/Tehran'))
-            print(str(p_mem) + "    time: " + t2.strftime("%Y-%m-%d %H:%M:%S") + "    delta: " + str(t2-t1) , file=f)
+            if( p_mem > mem_thr):
+                print("RAM : "+ f"{p_mem:<5.1f}" + "    time: " + t2.strftime("%Y-%m-%d %H:%M:%S") + "    delta: " + str(t2-t1) , file=f)
+            else:
+                print("cpu : "+ f"{p_cpu:<5.1f}" + "    time: " + t2.strftime("%Y-%m-%d %H:%M:%S") + "    delta: " + str(t2-t1) , file=f)
             t1 = t2
         os.system("pkill -f xray-linux")
 
